a2107224ef6920d3bb3c716a077b7bc8286ea7d9
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2022-09-13 23:50:52 +0700

    repair code n adding style on map data

--- src/App.js
+++ src/App.js
@@ -2,42 +2,65 @@ import './App.css'
 import { useState, useEffect } from 'react'
 import { getCurrency } from './api/service'
 
-
-
 function Card() {
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getCurrency()
-     .then((res) => {
+     .then(({data : res}) => {
         setData(res)
+        setLoading(false)
      }).catch(() => '')
   }, [])
 
+  const customStye = {
+    label: 'border text-center p-2',
+    text : 'text-dark fw-bolder',
+    
+  }
 
   return (
     <>  
     <div className='App'>
-      <table>
-        <thead>
-          <th>Currency</th>
-          <th>We Buy</th>
-          <th>Exchange Rate</th>
-          <th>We sell</th>
-        </thead>
-        <tbody>
-        {Array.isArray(data) && data?.map((item, i) => {
-          return (
-            <>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </>
-          )
-        })}
-        </tbody>
-      </table>
+      <div className='section'>
+        <div className='row'>
+          <div className='col-12'>
+            <table className='table shadow'>
+                <tr className=''>
+                  <th className={`${customStye.label} ${customStye.text}`}>No</th>
+                  <th className={`${customStye.label} ${customStye.text}`}>Currency Code</th>
+                  <th className={`${customStye.label} ${customStye.text}`}>Currency Name</th>
+                  <th className={`${customStye.label} ${customStye.text}`}>Icon</th>
+                  <th className={`${customStye.label} ${customStye.text}`}>Status</th>
+                </tr>
+                
+               {loading && (
+                  <>
+                    <div className='d-flex text-primary justify-content-center align-items-center position-absolute mt-4' style={{width: 200}}>
+                      <span className='spinner-border'></span> &nbsp; loading...
+                    </div>
+                  </>
+                )}
+
+                {!loading && (
+                Array.isArray(data) && data.slice(0, 7).map(({currencyCode, currencyName, icon, status}, i) => {
+                  return (
+                    <>
+                      <tr>
+                        <td className={`${customStye.label} ${customStye.text}`}>{i + 1}</td>
+                        <td className={`${customStye.label} text-primary`}>{currencyCode}</td>
+                        <td className={`${customStye.label} ${customStye.text}`}>{currencyName}</td>
+                        <td className={`${customStye.label} ${customStye.text}`}><img src={icon} aly='Icon' style={{width: 36}} /></td>
+                        <td className={`${customStye.label} text-success fw-bolder`}>{status}</td>    
+                      </tr>
+                   </>
+                  )
+                  }))}
+            </table>
+          </div>
+        </div>
+      </div>     
     </div>
     </>
   )
--- src/api/service.js
+++ src/api/service.js
@@ -6,6 +6,7 @@ const apiKey = '28970e639e0948eeab6a93a978b6840d'
 export function getCurrency() {
     return axios({
         method: 'get',
-        url: `https://api.currencyfreaks.com/latest?apikey=${apiKey}&symbols=IDR,GBP,EUR,JPY,CHF,CAD&base=IDR'`
+        // url: `https://api.currencyfreaks.com/latest?apikey=${apiKey}&symbols=IDR,GBP,EUR,JPY,CHF,CAD&base=IDR`
+        url: 'https://api.currencyfreaks.com/supported-currencies'
     })
 }
