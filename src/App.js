c61227bd4ad30042bb571f471d5c89cd9980146a
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2022-11-05 20:02:51 +0700

    init a App manifulate random user:

--- src/App.js
+++ src/App.js
@@ -1,78 +1,145 @@
-import './App.css'
-import { useState, useEffect } from 'react'
-import { getCurrency } from './api/service'
+import "./App.css";
+import { useState, useEffect } from "react";
+import { getUser, getAll } from "./api/service";
 
 function Card() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [dataUser, setDataUser] = useState({});
+  const [dataAllUser, setDataAllUser] = useState([]);
+  const [isClick, setIsClick] = useState({});
 
   useEffect(() => {
-    getCurrency()
-     .then(({data : res}) => {
-        setData(res)
-        setLoading(false)
-     }).catch(() => '')
-  }, [])
+    getUser()
+      .then(({ data: { results } }) => {
+        const convertToObj = results.reduce((a, b) =>
+          Object.assign({ a, b }, {})
+        );
+        setDataUser(convertToObj);
+      })
+      .catch(() => {});
 
-  const customStye = {
-    label: 'border text-center p-2',
-    text : 'text-dark fw-bolder',
-    
-  }
+    getAll()
+      .then(({ data: { results } }) => {
+        setDataAllUser(results);
+      })
+      .catch(() => {});
+  }, []);
+
+  console.log("data :", Object.keys(isClick).length);
 
   return (
-    <>  
-    <div className='App'>
-      <div className='section'>
-        <div className='row'>
-          <div className='col-12'>
-            <table className='table shadow'>
-                <tr className=''>
-                  <th className={`${customStye.label} ${customStye.text}`}>No</th>
-                  <th className={`${customStye.label} ${customStye.text}`}>Currency Code</th>
-                  <th className={`${customStye.label} ${customStye.text}`}>Currency Name</th>
-                  <th className={`${customStye.label} ${customStye.text}`}>Icon</th>
-                  <th className={`${customStye.label} ${customStye.text}`}>Status</th>
-                </tr>
-                
-               {loading && (
+    <>
+      <div className="App">
+        <div
+          className="card w-full shadow-sm m-3"
+          style={{
+            height: "30rem",
+            width: "40rem",
+            overflowY: "auto",
+          }}
+        >
+          <div
+            className="card-header mb-2 sticky-top d-flex flex-column"
+            style={{ backgroundColor: "white" }}
+          >
+            <div className="d-flex  flex-column">
+              {" "}
+              {dataUser && (
+                <>
+                  <div className="d-flex">
+                    <img
+                      src={
+                        Object.keys(isClick).length > 0
+                          ? isClick.picture.large
+                          : dataUser.picture?.large
+                      }
+                      alt={dataUser.title}
+                      className="img-thumbnail"
+                      style={{ width: "10vw" }}
+                    />
+                    <div></div>
+                    <div className="mx-3 d-flex flex-column">
+                      <div>
+                        Name :
+                        <span>{`${
+                          Object.keys(isClick).length > 0
+                            ? isClick.name?.first
+                            : dataUser.name?.first
+                        } ${
+                          Object.keys(isClick).length > 0
+                            ? isClick.name?.last
+                            : dataUser.name?.last
+                        }`}</span>
+                      </div>
+                      <div>
+                        Email :{" "}
+                        <span>
+                          {Object.keys(isClick).length > 0
+                            ? isClick.email
+                            : dataUser?.email}
+                        </span>
+                      </div>
+                      <div>
+                        Birthday :{" "}
+                        <span>
+                          {Object.keys(isClick).length > 0
+                            ? isClick.dob.date
+                            : dataUser.dob?.date}
+                        </span>
+                      </div>
+                      <div>
+                        Address :{" "}
+                        <span>{`${
+                          Object.keys(isClick).length > 0
+                            ? isClick.location?.street?.number
+                            : dataUser.location?.street?.number
+                        } ${
+                          Object.keys(isClick).length > 0
+                            ? isClick.location?.street?.name
+                            : dataUser.location?.street?.name
+                        }`}</span>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="card-body mt-2 d-flex text-align-center flex-wrap">
+            {Array.isArray(dataAllUser) &&
+              dataAllUser.map((item, index) => {
+                return (
                   <>
-                    <div className='d-flex text-primary justify-content-center align-items-center position-absolute mt-4' style={{width: 200}}>
-                      <span className='spinner-border'></span> &nbsp; loading...
+                    <div className="m-2">
+                      <img
+                        style={{ cursor: "pointer" }}
+                        src={item.picture.medium}
+                        alt={item.title}
+                        className="img-thumbnail"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setIsClick({ ...item });
+                        }}
+                      />
                     </div>
                   </>
-                )}
-
-                {!loading && (
-                Array.isArray(data) && data.slice(0, 7).map(({currencyCode, currencyName, icon, status}, i) => {
-                  return (
-                    <>
-                      <tr>
-                        <td className={`${customStye.label} ${customStye.text}`}>{i + 1}</td>
-                        <td className={`${customStye.label} text-primary`}>{currencyCode}</td>
-                        <td className={`${customStye.label} ${customStye.text}`}>{currencyName}</td>
-                        <td className={`${customStye.label} ${customStye.text}`}><img src={icon} aly='Icon' style={{width: 36}} /></td>
-                        <td className={`${customStye.label} text-success fw-bolder`}>{status}</td>    
-                      </tr>
-                   </>
-                  )
-                  }))}
-            </table>
+                );
+              })}
           </div>
+          <div className="card-footer">Footer...</div>
         </div>
-      </div>     
-    </div>
+      </div>
     </>
-  )
-
-} 
-
+  );
+}
 
 function App() {
   return (
-   <>
+    <>
+      <div className="d-flex justify-content-center m-3">
+        <span>Manipulate Random User</span>
+      </div>
       <Card />
-   </>
+    </>
   );
 }
 
--- src/api/service.js
+++ src/api/service.js
@@ -1,12 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
-// Ambil Key 
-const apiKey = '28970e639e0948eeab6a93a978b6840d'
+export function getAll() {
+  return axios({
+    method: "get",
+    url: "https://randomuser.me/api/?results=50",
+  });
+}
 
-export function getCurrency() {
-    return axios({
-        method: 'get',
-        // url: `https://api.currencyfreaks.com/latest?apikey=${apiKey}&symbols=IDR,GBP,EUR,JPY,CHF,CAD&base=IDR`
-        url: 'https://api.currencyfreaks.com/supported-currencies'
-    })
+export function getUser() {
+  return axios({
+    method: "get",
+    url: "https://randomuser.me/api/",
+  });
+}
+
+export function getUserFemale() {
+  return axios({
+    method: "get",
+    url: "https://randomuser.me/api/?results=500&gender=female",
+  });
+}
+
+export function getUserMale() {
+  return axios({
+    method: "get",
+    url: "https://randomuser.me/api/?results=500&gender=male",
+  });
 }
